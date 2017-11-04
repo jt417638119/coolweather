cc2b0e881d97afc095bb5c7f6c1e2642f785540d
jt417638119 <417638119@qq.com>
2017-11-04 10:54:33 +0800

    查询天气基本功能完成

--- app/src/main/java/com/coolweather/android/fragment/ChooseAreaFragment.java
+++ app/src/main/java/com/coolweather/android/fragment/ChooseAreaFragment.java
@@ -2,6 +2,7 @@ package com.coolweather.android.fragment;
 
 import android.app.Fragment;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,6 +15,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.coolweather.android.R;
+import com.coolweather.android.WeatherActivity;
 import com.coolweather.android.db.City;
 import com.coolweather.android.db.County;
 import com.coolweather.android.db.Province;
@@ -93,6 +95,13 @@ public class ChooseAreaFragment extends Fragment {
                }else if (currentLevle == LEVEL_CITY) {
                    selectedCity = cityList.get(position);
                    queryCounties();
+               }else if (currentLevle == LEVEL_COUNTY) {
+                   String weatherId = countyList.get(position).getWeatherId();
+                   Intent intent = new Intent(getActivity(), WeatherActivity.class);
+                   intent.putExtra("weather_id", weatherId);
+                   startActivity(intent);
+                   getActivity().finish();
+
                }
             }
         });
